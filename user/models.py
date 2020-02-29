bfe1be387b234b3df878531481787fca1fc72a64
kkkrx <769106369@qq.com>
2020-02-29 23:59:31 +0800

    2/29 config the relate between User and Profile

--- user/models.py
+++ user/models.py
@@ -7,6 +7,9 @@ from django.db import models
 
 
 class User(models.Model):
+    """
+    用户数据模型
+    """
     SEX = (
         ('M', '男'),
         ('F', '女'),
@@ -31,5 +34,36 @@ class User(models.Model):
         times = today - birthDate
         return times.days // 365
 
+    @property
+    def profile(self):
+        """
+        该用户的配置项
+        """
+        if not hasattr(self, 'myProfile'):
+            myProfile, created = Profile.objects.get_or_create(id=self.id)
+            self.myProfile = myProfile
+        return self.myProfile
 
 
+class Profile(models.Model):
+    """
+    用户配置项
+    """
+    SEX = (
+        ('M', '男'),
+        ('F', '女'),
+    )
+
+    location = models.CharField(max_length=32, verbose_name='目标城市')
+
+    minDistance = models.IntegerField(default=1, verbose_name='最小查找范围')
+    maxDistance = models.IntegerField(default=10, verbose_name='最大查找范围')
+
+    minDatingAge = models.IntegerField(default=18, verbose_name='最小交友年龄')
+    maxDatingAge = models.IntegerField(default=45, verbose_name='最大交友年龄')
+
+    datingSex = models.CharField(max_length=8, choices=SEX, verbose_name='匹配的性别')
+
+    vibration = models.BooleanField(default=True, verbose_name='是否开启震动')
+    onlyMat = models.BooleanField(default=True, verbose_name='不让未匹配的人看我的相册')
+    autoPlay = models.BooleanField(default=True, verbose_name='是否自动播放视频')
